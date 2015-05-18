e72d27c7c035917b9c4e40fa3c7e6b4bf8b647fa
boruoshihao <boruoshihao@gmail.com>
2015-05-18 15:51:59 -0400

    Add two headers: inc/use_bl_cpu.h inc/use_bl_magma.h

---
+++ inc/use_bl_cpu.h
@@ -0,0 +1,11 @@
+#ifndef USE_BL_CPU
+#define USE_BL_CPU
+
+#define gmm          gmm_cpu
+#define eigen        eigen_cpu
+#define LUconstruct  LUconstruct_cpu 
+#define inverse      inverse_cpu
+#define solve_lineq  solve_lineq_cpu
+#define QRMatrix     QRMatrix_cpu
+
+#endif
---
+++ inc/use_bl_magma.h
@@ -0,0 +1,11 @@
+#ifndef USE_BL_MAGMA
+#define USE_BL_MAGMA
+
+#define gmm          gmm_magma
+#define eigen        eigen_magma
+#define LUconstruct  LUconstruct_magma
+#define inverse      inverse_magma
+#define solve_lineq  solve_lineq_magma
+#define QRMatrix     QRMatrix_magma
+
+#endif

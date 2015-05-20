6945aa8cfd7d57f05ba0ae00104590d0c5e74b98
boruoshihao <boruoshihao@gmail.com>
2015-05-20 11:59:31 -0400

    Add Warning in inc/use_bl_cpu.h and inc/use_bl_magma.h.

--- inc/use_bl_cpu.h
+++ inc/use_bl_cpu.h
@@ -1,6 +1,11 @@
 #ifndef USE_BL_CPU
 #define USE_BL_CPU
 
+//WARNING: it is not safe to use this header
+//Another way to swith between cpu and gpu is: 
+//#define BL_NAME(x) x##_cpu
+//call gmm_cpu by: BL_NAME(gmm)
+
 #define gmm          gmm_cpu
 #define eigen        eigen_cpu
 #define LUconstruct  LUconstruct_cpu 
--- inc/use_bl_magma.h
+++ inc/use_bl_magma.h
@@ -1,6 +1,11 @@
 #ifndef USE_BL_MAGMA
 #define USE_BL_MAGMA
 
+//WARNING: it is not safe to use this header
+//Another way to swith between cpu and gpu is: 
+//#define BL_NAME(x) x##_magma
+//call gmm_magma by: BL_NAME(gmm)
+
 #define gmm          gmm_magma
 #define eigen        eigen_magma
 #define LUconstruct  LUconstruct_magma
